62403af72cf741d58a24d33dce3d6674d60eee47
JettLing18 <165157188+JettLing18@users.noreply.github.com>
2026-05-05 09:39:14 +0800

    Activity Lab7 Commit

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,20 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function dashboard()
+    {
+        $user = Auth::user();
+
+
+        if ($user->role == 1) {
+        
+            $notes = Note::with('user')->latest()->get();
+            return view('admin.dashboard', compact('notes'));
+        }
+
+
+        $notes = $user->notes;
+        return view('dashboard', compact('notes'));
+    }
+
 }
